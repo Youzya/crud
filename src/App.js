9c2fab6df7d26fb10e2ca32a1c527f2202ebbba7
Youzya <holodilnikred@gmail.com>
2024-07-21 14:09:21 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Notes from './components/Notes';
+
+function App() {
+    return (
+        <div className="container">
+            <Notes />
+        </div>
+    );
+}
+
+export default App;
